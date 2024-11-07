be348511d4e27ba90886ed2b29ed3cf058ae146d
AndreyKuklinov <37548463+AndreyKuklinov@users.noreply.github.com>
2024-11-07 05:59:19 +0500

    Comment unused lines

--- src/components/OfferList.tsx
+++ src/components/OfferList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import OfferCard from './OfferCard';
 import { Offer } from '../types';
 
@@ -7,25 +7,24 @@ interface OfferListProps {
 }
 
 function OfferList({ offers }: OfferListProps) {
-  // (Пока мы никак не используем активную карточку)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [activeOfferId, setActiveOfferId] = useState<number | null>(null);
+  // Все строки, связанные с обновлением активной карточки закомментированы, чтобы не мучить Lint неиспользуемой переменной
+  // const [activeOfferId, setActiveOfferId] = useState<number | null>(null);
 
-  const handleMouseEnter = (offerId: number) => {
-    setActiveOfferId(offerId);
-  };
+  // const handleMouseEnter = (offerId: number) => {
+  //   setActiveOfferId(offerId);
+  // };
 
-  const handleMouseLeave = () => {
-    setActiveOfferId(null);
-  };
+  // const handleMouseLeave = () => {
+  //   setActiveOfferId(null);
+  // };
 
   return (
     <div className='cities__places-list places__list tabs__content'>
       {offers.map((offer) => (
         <div
           key={offer.title}
-          onMouseEnter={() => handleMouseEnter(offer.id)}
-          onMouseLeave={handleMouseLeave}
+          // onMouseEnter={() => handleMouseEnter(offer.id)}
+          // onMouseLeave={handleMouseLeave}
         >
           <OfferCard offer={offer} />
         </div>
