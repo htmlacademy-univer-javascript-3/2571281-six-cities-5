9b6b663fd4013cd45c78a893319b4f76d77ca0bc
AndreyKuklinov <37548463+AndreyKuklinov@users.noreply.github.com>
2025-01-11 23:37:08 +0500

    Fix counter

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { fetchOffers, initializeAuth } from './store/api-actions';
+import { fetchFavorites, fetchOffers, initializeAuth } from './store/api-actions';
 import App from './App';
 
 store.dispatch(initializeAuth());
 store.dispatch(fetchOffers());
+store.dispatch(fetchFavorites());
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
